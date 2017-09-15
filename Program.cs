2283e263b3dcc15befdcf72cbd29f1d599cae9d6
RenaTin1 <31945448+RenaTin1@users.noreply.github.com>
2017-09-14 23:53:49 -0400

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,29 +4,54 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MathChallenge
+namespace deliverable_1
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int num1;
-            int num2;
-            int result;
-
-            Console.WriteLine("Enter a three digit number");
-            num1 = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine("Enter a second three digit number");
-            num2 = Convert.ToInt32(Console.ReadLine());
-
-            result = num1 + num2;
-
-            Console.WriteLine("The sum of the two nunbers is: " + result);
-            Console.ReadLine();
-
+            AddInts();
             Console.ReadKey();
 
+
+        }
+        public static bool AddInts()
+        {
+            //Declare variable.  User needs to input two integers
+            int Num1, Num2;
+
+            //Ask user to enter digits
+            Console.WriteLine("Enter a number of that consists of three digits: ");
+            Num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter a second number that consits of three digits: ");
+            Num2 = Convert.ToInt32(Console.ReadLine());
+
+            string digitCount1 = Num1.ToString();
+            string digitCount2 = Num2.ToString();
+
+            if (digitCount1.Length == digitCount2.Length)
+            {
+                Console.WriteLine("Correct! Now let's find the sum of both numbers!");
+            }
+
+            else
+            {
+                Console.WriteLine("These number are not equal, try again!");
+            }
+
+            int digit1 = Convert.ToInt32(digitCount1[0]) + Convert.ToInt32(digitCount2[0]);
+            int digit2 = Convert.ToInt32(digitCount1[1]) + Convert.ToInt32(digitCount2[1]);
+            int digit3 = Convert.ToInt32(digitCount1[2]) + Convert.ToInt32(digitCount2[2]);
+
+            if ((digit1 == digit2) && (digit2 == digit3) && (digit1 == digit3))
+            {
+                return true;
+            } 
+               else
+             {
+                return false;
+            }
         }
     }
 }
